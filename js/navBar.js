8cc8ba37fb0c19e0a65e6c5508723d077f630ab1
KimWithSomething <87281392+KimWithSomething@users.noreply.github.com>
2022-06-28 20:49:56 -0500

    Deleted files/ updated some stuff
    *deleted unused files
    *updated NavBar
    *created htaccess

--- js/navBar.js
+++ js/navBar.js
@@ -6,13 +6,13 @@ var navBar = `
 				<img src="images/SOE_logo.png">
 			</td>
             <td>
-                <a href="index.html"><button class="menuButton">Home page</button></a>
+                <a href="indeX.html"><button class="menuButton">Home page</button></a>
             </td>
 			<td>
 				<a href="release_logs.html"><button class="menuButton">Release Logs</button></a>
 			</td>
             <td>
-                <a href="downloads.html"><button class="menuButton">Downloads</button></a>
+                <a href="downloadS.html"><button class="menuButton">Downloads</button></a>
             </td>
             <td>
                 <a href="Contributors.html"><button class="menuButton">Contributors</button></a>
